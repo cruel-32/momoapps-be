ac00f35ee47a5a135a2ab77e40a1373ec8b80186
cruel32 <cruel32@nate.com>
2023-03-07 22:57:53 +0900

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmConfig } from './config';
+import { UsersModule } from './modules/users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
