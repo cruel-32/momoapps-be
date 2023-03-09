21cd2c4a0ce04e85ed15dad3b77dded39aacf55b
cruel32 <cruel32@nate.com>
2023-03-09 21:45:33 +0900

    기본셋팅

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { TypeOrmConfig } from './config';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmConfig),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+    CategoriesModule,
     UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
